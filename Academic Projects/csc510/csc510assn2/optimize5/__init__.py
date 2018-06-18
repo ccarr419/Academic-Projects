57c64dd9a978e1a33b1e6eb1a27a160f29c6cea4
ccarr419 <ccarr419@live.kutztown.edu>
2018-06-18 19:01:35 -0400

    CleanUp
    Removed all unnecessary files which included duplicates, backup files,
    unrelated files and files left over from compilation

--- Academic Projects/csc510/csc510assn2/optimize5/__init__.py
+++
@@ -1,4 +0,0 @@
-# optimize5/__init__.py
-# package import initialization
-# CSC 526, Spring 2012, D. Parson
-__all__ = ["progcalc", "TypeChecks", "CodeGenerator", "TreeOptimizer"]
--- Academic Projects/csc510/jitstm2017/__init__.py
+++
@@ -1,4 +0,0 @@
-# jitstm2017/__init__.py
-# package import initialization
-# D. Parson, Fall 2013, 2015, 2017
-__all__ = ["diffset", "crunchlog"]
